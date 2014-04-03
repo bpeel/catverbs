02888ec9a450f28ccbf77f4ec172a247b058fb39
Neil Roberts <bpeeluk@yahoo.co.uk>
2014-04-03 11:01:55 +0100

    Add some little bash functions for manipulating the data

---
+++ bash-snippets.sh
@@ -0,0 +1,20 @@
+function groups() {
+  # Sort the groups from the todo list in reverse order of frequency
+  sed -r s/'^([^|]*)\|.*/\1/' verbs-todo.txt | sort | uniq -c | sort -k 1,1n
+}
+
+function remove_group {
+  # Remove a group from the todo list
+  sed -ni '/^'"$1"'|\([^|]\+\)$/! p' verbs-todo.txt; git add verbs-todo.txt
+}
+
+function generate_group {
+  if test "$#" -lt 3; then
+    echo "usage: generate_group <group_name> <parent> <infinitive_suffix>";
+  fi
+  # Generate files for a group
+  for x in `sed -n 's/^'"$1"'|\([^|]\+\)$/\1/p' < verbs-todo.txt `; do
+      echo -e "stem=$x\nparent=$2" > data/"$x$3".txt;
+      git add data/"$x$3".txt;
+  done
+}
