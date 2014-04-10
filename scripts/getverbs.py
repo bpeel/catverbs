0d18d9d05df2efb2b24b04ea886e06f0fbb5f847
Neil Roberts <bpeeluk@yahoo.co.uk>
2014-04-10 16:43:57 +0100

    Change the getverbs script to only use the list from DEIC
    Otherwise there are a lot of archaic verbs that we probably don't
    want.

--- scripts/getverbs.py
+++ scripts/getverbs.py
@@ -16,16 +16,88 @@
 import xml.etree.ElementTree as ET
 import sys
 import re
+import os
+
+NAMESPACE = "http://www.mediawiki.org/xml/export-0.8/"
+
+def get_page_text(page):
+    text_element = page.find("./{" + NAMESPACE + "}revision/"
+                             "{" + NAMESPACE + "}text")
+    return ''.join(text_element.itertext())
+
+def get_verb_list_text(root):
+    for page in root.findall(".//{" + NAMESPACE + "}page"):
+        title_element = page.find("./{" + NAMESPACE + "}title")
+        title = ''.join(title_element.itertext())
+        if title == "Viccionari:Llista dels verbs catalans":
+            return get_page_text(page)
+
+def get_verb_list(root):
+    text = get_verb_list_text(root)
+    return re.findall(r'^\* *\[\[([^\]]+?)(?:-se(?:'+"'"+'n)?)?\]\] *$',
+                      text,
+                      re.MULTILINE)
+
+class Verb:
+    def __init__(self, name):
+        self.name = name
+        self.done = False
+        self.have_page = False
+        self.in_list = False
 
 e = ET.parse(sys.stdin)
+
+verbs = {}
 
 verb_re = re.compile(r'\{\{-verb-\|ca(?:talà)?\}\}')
 group_re = re.compile(r'{{ca\.v\.conj\.(.*)}}')
 
-for text in e.findall(".//{http://www.mediawiki.org/xml/export-0.8/}text"):
-    content = ''.join(text.itertext())
+data_dir = os.path.join(os.path.split(__file__)[0], '..', 'data')
 
-    if verb_re.search(content):
-        m = group_re.search(content)
+for verb_name in get_verb_list(e):
+    verb = Verb(verb_name)
+    verbs[verb_name] = verb
+    file = os.path.join(data_dir, verb_name + '.txt')
+    verb.in_list = True
+    verb.group = None
+
+data_re = re.compile(r'^(.*)\.txt$')
+
+for file_name in os.listdir(data_dir):
+    m = data_re.match(file_name)
+    if m is not None:
+        verb_name = m.group(1)
+        if verb_name not in verbs:
+            verbs[verb_name] = Verb(verb_name)
+        verbs[verb_name].done = True
+
+for page in e.findall(".//{" + NAMESPACE + "}page"):
+    title_element = page.find("./{" + NAMESPACE + "}title")
+    title = ''.join(title_element.itertext())
+
+    if title not in verbs:
+        continue
+
+    verb = verbs[title]
+    verb.have_page = True
+
+    text = get_page_text(page)
+
+    if verb_re.search(text):
+        m = group_re.search(text)
         if m:
-            print(m.group(1))
+            verb.group = m.group(1)
+
+for verb_name in sorted(verbs):
+    verb = verbs[verb_name]
+    if verb.in_list:
+        if verb.done:
+            continue
+        print('+ ' + verb_name, end='')
+        if verb.group:
+            print(' ' + verb.group, end='')
+        print()
+    elif verb.done:
+        print('! ' + verb_name)
+    else:
+        print('? ' + verb_name)
